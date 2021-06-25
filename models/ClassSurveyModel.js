5ed1fc1a5ccc980eccd057addb4bab1490a87a7e
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-06-25 09:42:30 -0700

    added update static method

--- models/ClassSurveyModel.js
+++ models/ClassSurveyModel.js
@@ -10,10 +10,35 @@ class ClassSurveyModel {
     static async getAllTopicData() {
         try {
             const response = await db.any(
-                `SELECT topic_name, ranking_title FROM topics
+                `SELECT * FROM topics
                     INNER JOIN rankings
                     ON topics.topic_score = rankings.id
                 ORDER BY topics.topic_name;`
+            )
+            return response;
+        } catch (error) {
+            console.error("ERROR: ", error);
+            return error;
+        }
+    }
+
+    static async getAllRankings() {
+        try {
+            const response = db.any(
+                `SELECT * FROM rankings;`
+            )
+            return response;
+        } catch (error) {
+            console.error("ERROR: ", error);
+            return error;
+        }
+    }
+
+    static async updateRanking(topic, topic_score) {
+        try {
+            const response = await db.result(
+                `UPDATE topics SET topic_score = $1 
+                WHERE topic_name=$2;`, [topic_score, topic]
             )
             return response;
         } catch (error) {
